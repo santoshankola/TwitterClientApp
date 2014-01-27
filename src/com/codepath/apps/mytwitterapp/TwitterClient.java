cfc7e63c8ef491f1466ac1a743702355f5ccf353
Santosh Ankola <ankola@yahoo-inc.com>
2014-01-27 12:33:20 -0800

    initial commit

--- src/com/codepath/apps/mytwitterapp/TwitterClient.java
+++ src/com/codepath/apps/mytwitterapp/TwitterClient.java
@@ -27,7 +27,7 @@ public class TwitterClient extends OAuthBaseClient {
     public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
     public static final String REST_CONSUMER_KEY = "4pnDrbi4U4dsji1nxW3R6A";       // Change this
     public static final String REST_CONSUMER_SECRET = "lDfiLpiVooFHsKN6bgJ1qK32ssiEPD0kYZ6k5FrYQY"; // Change this
-    public static final String REST_CALLBACK_URL = "oauth://mytwitterapp"; // Change this (here and in manifest)
+    public static final String REST_CALLBACK_URL = "oauth://ex"; // Change this (here and in manifest)
     
     public TwitterClient(Context context) {
         super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
